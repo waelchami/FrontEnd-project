d9acaa552d44a1b460548dc237905ceabbe329af
waelchami <waelchami51@gmail.com>
2025-11-23 16:05:41 +0200

    Add files via upload

---
+++ phone1.js
@@ -0,0 +1,23 @@
+let a = document.querySelector('#a');
+let a1 = document.querySelector('#a1');
+let a2 = document.querySelector('#a2');
+let a3 = document.querySelector('#a3');
+let a4 = document.querySelector('#a4');
+
+
+
+// function changeBackground(backgr) {
+    // let Link = document.getElementsByTagName("a");
+    // for (let i = 0; i < Link.length; i++) {
+        // Link[i].style.backgroundColor = "";
+		// Link[i].style.color = "";
+		
+    // }
+    // let chgback = document.getElementById(backgr);
+    // chgback.style.backgroundColor = "#fff";
+	// chgback.style.color = "#000";
+// }
+
+    a1.style.backgroundColor = "#fff";
+    a1.style.color = "#000";
+	
---
+++ phone3.js
@@ -0,0 +1,42 @@
+ let a = document.querySelector('#a');
+let a1 = document.querySelector('#a1');
+let a2 = document.querySelector('#a2');
+let a3 = document.querySelector('#a3');
+let a4 = document.querySelector('#a4');
+
+
+
+// function backg(color){
+	// a.style.background = color;
+
+// }
+// function backg1(color){
+	// a1.style.background = color;
+// }
+// function backg2(color){
+	// a2.style.background = color;
+// }
+// function backg3(color){
+	// a3.style.background = color;
+// }
+// function backg4(color){
+	// a4.style.background = color;
+	// a4.style.color = black;
+// }
+
+
+
+function changeBackground(backgr) {
+    let Link = document.getElementsByTagName("a");
+    for (let i = 0; i < Link.length; i++) {
+        Link[i].style.backgroundColor = "";
+		Link[i].style.color = "";
+		
+    }
+    let chgback = document.getElementById(backgr);
+    chgback.style.backgroundColor = "#fff";
+	chgback.style.color = "#000";
+}
+
+    a3.style.backgroundColor = "#fff";
+    a3.style.color = "#000";
